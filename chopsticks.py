8ad8064b9af5098e9c47de17d7185ea7805b38da
Paul Ohm <ohm@law.georgetown.edu>
2016-07-01 11:04:59 -0400

    First version with AI. It works (I think) but it is unacceptably inefficient. Need to build in all sorts of efficiencies. (Most importantly, should switch from depth first to breadth first trees to look for sure victories.)

--- chopsticks.py
+++ chopsticks.py
@@ -4,22 +4,136 @@ from copy import copy
 class Chopsticks(object):
 
     def __init__(self):
-        self.state = [1, 1, 1, 1]
+        #self.state = [1, 1, 1, 1]
+        self.state = [1, 4, 0, 1]
         self.p1sturn = True
 
     def show(self):
         showpair.show(self.state)
 
+    def ai(self):
+        # Given the current state, and assuming it is the CPU's turn,
+        # start the minimax algorithm
+        return(self.minimax(self.state, self.p1sturn, []))
+
+    def score(self, state):
+        if state[0] + state[1] == 0:
+            return -10
+        elif state[2] + state[3] == 0:
+            return 10
+        else:
+            return 0
+
+    def state_seed2 (self, state):
+        # Returns a compact integer reflecting the current state of the board.
+        # Algorithm: Use prime numbers to encode four slots
+        multipliers = [2, 3, 5, 7]
+        total = 1
+        
+        for x,y in zip(state, multipliers):
+            # Must +1 to convert 0 to 1
+            total = total * (x + 1) * y
+            
+        return total
+
+    def state_seed (self, state):
+        # Turns a list into a four digit number reflecting the current state of the board.
+        return int(''.join([str(i) for i in state]))
+
+    def minimax(self, state, p1sturn, seen):
+        print ("\ntop of minimax")
+        print (state, p1sturn, seen)
+        score = self.score(state)
+        if score != 0:
+            print ("* Score is " + str(score) + ". Branch terminating.\n")
+            return (score, "")
+
+        seed = self.state_seed(state)
+        print (seed)
+
+        if seed in seen:
+            # Loop, so return a 0
+            print ("* Loop! Branch terminating.\n")
+            return (0, "")
+        
+        newseen = copy(seen)
+        newseen.append(seed)
+
+        mapper = {
+            'a' : 2,
+            's' : 3,
+            'q' : 0,
+            'w' : 1
+        }
+
+        moves = []
+        scores = []
+
+        # Generate list of possible moves for current player
+        if (p1sturn):
+            moves = ["qa", "qs", "wa", "ws"]
+            for hand in [("q", "w"), ("w", "q")]:
+                for i in range(0, state[mapper[hand[0]]]):
+                    moves.append(hand[0] + hand[1] + str(i + 1))
+        else:
+            # p2sturn
+            moves = ["aq", "sq", "aw", "sw"]
+            for hand in [("a", "s"), ("s", "a")]:
+                for i in range(0, state[mapper[hand[0]]]):
+                    moves.append(hand[0] + hand[1] + str(i + 1))
+        print ("If this were AI, this would be the moves:")
+        print (moves)
+        print ("And this is the seen list")
+        print (newseen)
+
+        # Starting to recurse
+        for move in moves:
+            print("Trying move " + move + " with state ")
+            print(state)
+            nextstate = self.testmove(state, move, p1sturn)
+            if (nextstate[0] > -1):
+                print ("returned a valid board. recurse on it.")
+                scores.append(self.minimax(nextstate, not p1sturn, newseen)[0])
+            else:
+                # Invalid move, so make score for this one just 0
+                print ("invalid move")
+                print (nextstate[1])
+                print ("* Branch terminating.\n")
+                scores.append(0)
+                
+        print ("Scores are:")
+        print (scores)
+        print ("Moves are:")
+        print (moves)
+
+        # Figure out which to return. Return should be the move chosen.
+        if (p1sturn):
+            win_func = max
+        else:
+            win_func = min
+            
+        win_val = win_func(scores)
+        win_index = scores.index(win_val)
+        print ("** Done with branch. Best move for current player is " + str(moves[win_index]) + " with value = " + str(win_val) + "\n\n")
+        return (win_val, moves[win_index])
+
     def next(self):
         # Play the next turn
+        
+        # Just temporary:
+        best_move = self.ai()
+        print ("*** Best move seems to be " + best_move[1])
+
+        print ("seed is " + str(self.state_seed(self.state)))
+        
         # If it is a human's turn, prompt them for their move, verify it is a valid move, and change the state
         if self.p1sturn:
             print ("P1", end="")
         else:
             print ("P2", end="")
         move = input ("> ")
-        newstate = self.testmove(move)
-        if (newstate[0] > 0):
+        newstate = self.testmove(self.state, move, self.p1sturn)
+        if (newstate[0] > -1):
             self.state = copy(newstate)
             self.show()
             self.p1sturn = not self.p1sturn
@@ -38,7 +152,7 @@ class Chopsticks(object):
         print ("Thx for plzying!!!")
         exit()
             
-    def testmove(self, move):
+    def testmove(self, state, move, p1sturn):
         # The internal representation of a move is a two letter combination
         # with an optional number
         # The letters refer to following square on the keyboard:
@@ -53,7 +167,7 @@ class Chopsticks(object):
         # as3 = from bottom left to bottom right (bump)
         # In the final example, the 3 is the number of fingers you are bumping
 
-        # This function returns a newstate from self.state and move
+        # This function returns a newstate from state and move
         # It does error checking to ensure this is a valid move
 
         # Return value is either a newstate (presumed valid)
@@ -63,7 +177,6 @@ class Chopsticks(object):
         # This is complex given the representation for a move
         sort_move = move
         sort_move = ''.join(sorted(sort_move))
-        print("sort_move is " + sort_move)
         if sort_move == "aq" or sort_move == "sw" or sort_move == "aw" or sort_move == "qs":
             move_type = "tap"
         else:
@@ -84,7 +197,7 @@ class Chopsticks(object):
         }
 
         # Test 0: Make sure the right player has played
-        if self.p1sturn:
+        if p1sturn:
             if not (from_hand == 'q' or from_hand == 'w'):
                 return [-3, "It's Player One's turn."]
         else:
@@ -94,36 +207,37 @@ class Chopsticks(object):
         # Other tests, depending on tap or bump
         if move_type == "tap":
             # Test 1, if "from" hand is zero, error
-            if self.state[mapper[from_hand]] == 0:
+            if state[mapper[from_hand]] == 0:
                 return [-1, "Invalid tap: 'from' hand is zero."]
             
             # Test 2, if "to" hand is zero, error
-            if self.state[mapper[to_hand]] == 0:
+            if state[mapper[to_hand]] == 0:
                 return [-1, "Invalid tap: 'to' hand is zero."]
             
         elif move_type == "bump":
             
             # Test 1: disallow the 1 -> 0 bump
-            if (self.state[mapper[from_hand]] + self.state[mapper[to_hand]]) == 1:
+            if (state[mapper[from_hand]] + state[mapper[to_hand]]) == 1:
                 return [-2, "Invalid bump: bump with only one finger"]
 
             # Test 2: The from hand must be at least one
-            if self.state[mapper[from_hand]] == 0:
+            if state[mapper[from_hand]] == 0:
                 return [-2, "Invalid bump: bump 'from' is zero."]
 
             # Test 3: The from hand must have at least bumped fingers
-            if self.state[mapper[from_hand]] < bumped:
+            if state[mapper[from_hand]] < bumped:
                 return [-2, "Invalid bump: Not enough fingers on 'from' hand."]
 
             # Test 4: The to hand plus bumped must not be greater than 4
-            if self.state[mapper[to_hand]] + bumped > 4:
+            if state[mapper[to_hand]] + bumped > 4:
                 return [-2, "Invalid bump: Overflow after bump."]
 
         # Got here, so no errors in move. Calculate and return the newstate
-        newstate = copy(self.state)
+        newstate = copy(state)
         if move_type == "tap":
             newstate[mapper[to_hand]] += newstate[mapper[from_hand]]
         elif move_type == "bump":
+            print ("In bump.")
             newstate[mapper[to_hand]] += bumped
             newstate[mapper[from_hand]] -= bumped
 
@@ -132,20 +246,3 @@ class Chopsticks(object):
 
         return newstate
                           
-game = Chopsticks()
-game.show()
-while True:
-    game.next()
-# game.next()
-# print ("aq")
-# game.testmove("aq")
-# game.show()
-# print("wa")
-# game.testmove("wa")
-# game.show()
-# print ("qw")
-# game.testmove("qw")
-# game.show()
-# print("as")
-# game.testmove("as")
-# game.show()
---
+++ doit.py
@@ -0,0 +1,19 @@
+import chopsticks
+
+game = chopsticks.Chopsticks()
+game.show()
+while True:
+    game.next()
+# game.next()
+# print ("aq")
+# game.testmove("aq")
+# game.show()
+# print("wa")
+# game.testmove("wa")
+# game.show()
+# print ("qw")
+# game.testmove("qw")
+# game.show()
+# print("as")
+# game.testmove("as")
+# game.show()
